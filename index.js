dfc8ae072ad7f81f6e870e20bc3ab7a3e00adca4
Salsabeel Salah <salsabeelsay@gmail.com>
2018-05-30 16:01:30 +0200

    editing index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-alert("Hello");
+var http  = require('http');
+
+http.createServer(function(req,res){
+    res.writeHead(200,{'content-type':'text/html'});
+    res.write('Hello');
+    res.end();
+}).listen(8080);
 /*exports.helloHttp = function helloHttp(req, res){
     res.json({fulfillmentText : 'This is a sample response from your webhook'});
 };*/
