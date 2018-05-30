1fefcc3c01554b6733924cdf2168b55f1612a885
Salsabeel Salah <salsabeelsay@gmail.com>
2018-05-30 15:59:04 +0200

    editing index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-exports.helloHttp = function helloHttp(req, res){
+alert("Hello");
+/*exports.helloHttp = function helloHttp(req, res){
     res.json({fulfillmentText : 'This is a sample response from your webhook'});
-};
+};*/
